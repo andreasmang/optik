1d8725f3b95adbfe289e4d8b308cff7177ba6dd5
Andreas Mang <andreas@math.uh.edu>
2019-09-18 16:52:55 -0500

    lsq example updated

--- examples/exGradDesLSQ.m
+++ examples/exGradDesLSQ.m
@@ -1,22 +1,33 @@
 clear, close all; clc;
 
+% this example illustrates how to solve the
+% (unconstrained) least squares problem
+%
+%   minimize 0.5*||A*x - b||_2^2
+%
+% using a gradient descent approach
+
+% define matrix
 B = [1,1,1; 1,2,3];
 A = B'*B + 0.001*eye(3);
 
+% define true solution
 xtrue = [1;2;3];
 
+% compute right hand side (with noise added)
 eta = randn(3,1);
 b   = A*xtrue + 0.01*eta;
 
+% define function handle for objective function
 objfun = @(x) objFunLSQ(A,x,b);
 
+% compute initial guess x0 = 0
 x0 = zeros( size(xtrue) );
 
-[fc,gc] = objfun( x0 );
-
+% run gradient descent algorithm
 x = gradientDescent( objfun, x0 );
 
-
+% display results for users
 fprintf('computed solution: (%e,%e,%e)\n', x);
 fprintf('true solution:     (%e,%e,%e)\n', xtrue);
 fprintf('relative error:    %e\n', norm(xtrue(:) - x(:))/norm(x(:)));
