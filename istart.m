71e3ae9e89c8a5f0234d7f3b51501f9f3db1b72f
Andreas Mang <andreas@math.uh.edu>
2020-08-17 13:09:11 -0500

    startup script modified

--- istart.m
+++ istart.m
@@ -1,9 +1,17 @@
+clear, close all;
+clc;
+
+addpath( 'kernel' );
+addpath( 'numerics' );
+addpath( 'helper' );
+addpath( genpath('scripts') );
+
+
+
+
 %######################################################
 % This code is part of the Matlab-based toolbox
 % OPTIK --- Optimization Toolkit
 % For details see https://github.com/andreasmang/optik
 %######################################################
-clear, close all;
-clc;
 
-addpath('data','examples','kernel','homework','viz','numerics');
