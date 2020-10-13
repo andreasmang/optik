363c25533d192f7ed2eb3137834c440e0cd1c01e
Andreas Mang <andreas@math.uh.edu>
2020-10-12 22:44:30 -0500

    comment fixed

--- scripts/examples/exRBNewtDesc.m
+++ scripts/examples/exRBNewtDesc.m
@@ -108,9 +108,8 @@ end
 
 
 
-%#######################################################
+%######################################################
 % This code is part of the Matlab-based toolbox
-% MACHINE --- MAthematical and Computational metHods
-% for INverse problEms
-% For details see https://github.com/andreasmang/machine
-%#######################################################
+% OPTIK --- Optimization Toolkit
+% For details see https://github.com/andreasmang/optik
+%######################################################
